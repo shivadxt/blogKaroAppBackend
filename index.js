0fa5269913bd429eaae45b64f4fd7eb8d551b30c
shivadxt <shivamweb3.0@gmail.com>
2024-01-08 23:11:50 +0530

    MongoDB-Atlas Connection setup

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import  express  from "express"
 import router from "./routes/user-route.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:Shivam1234@blogkaro.j6psaza.mongodb.net/?retryWrites=true&w=majority')
+.then(()=> app.listen(2000))
+.then(()=> console.log('Database is connected to server !'))
+.catch((err)=> console.log(err));
 
 app.get('/',(req,res)=>{
     res.send("Hello From Home!");
 });
 
-app.listen(2000,()=>{console.log("Server is Running !")});
+// app.listen(2000,()=>{console.log("Server is Running !")});
